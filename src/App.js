e4aa6c1da7fa9bb05581c1aac9937ea272e454b7
Panagiotis Fanartzis <eleinos100@hotmail.com>
2018-04-08 17:22:07 +0300

    added provider to pass the store in the application

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import store from './store/';
+import {Provider} from 'react-redux';
 
+import store from './store/index';
 import Navbar from './Navbar/Navbar';
 import Home from './Pages/Home/Home';
 import Omdb from './Pages/Omdb/Omdb';
@@ -13,14 +14,16 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <Navbar store={store}/>
-          <Route exact path ="/" component={Home} />
-          <Route exact path ="/omdb" component={Omdb} />
-          <Route exact path ="/weather" component={Weather} />
-          <Route exact path ="/github" component={Github} />
-          <Route exact path ="/weather-forecast" component={WeatherForecast} />
-        </div>
+        <Provider store={store}>
+          <div>
+            <Navbar />
+            <Route exact path ="/" component={Home} />
+            <Route exact path ="/omdb" component={Omdb} />
+            <Route exact path ="/weather" component={Weather} />
+            <Route exact path ="/github" component={Github} />
+            <Route exact path ="/weather-forecast" component={WeatherForecast} />
+          </div>
+        </Provider>
       </Router>
     );
   }
